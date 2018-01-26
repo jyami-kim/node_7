7152298f9b0e0512791a4b7952eafb2bdb839a30
mjung1798 <mor2222@naver.com>
2018-01-26 23:10:36 +0900

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,170 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var database;
+var UserSchema;
+var UserModel;
 
-module.exports = router;
+//데이터베이스 객체, 스키마 객체, 모델 객체를 이 모듈에서 사용할 수 있도록 전달함
+var init = function(db, schema, model){
+  console.log('init호출됨.');
+
+  database = db;
+  UserSchema = schema;
+  UserSchema = model;
+}
+
+
+var login = function(req, res) {
+	console.log('user 모듈 안에 있는 login 호출됨.');
+
+	// 요청 파라미터 확인
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+	
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+	
+    // 데이터베이스 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
+	if (database) {
+		authUser(database, paramId, paramPassword, function(err, docs) {
+			// 에러 발생 시, 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 로그인 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 로그인 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+			
+            // 조회된 레코드가 있으면 성공 응답 전송
+			if (docs) {
+				console.dir(docs);
+
+                // 조회 결과에서 사용자 이름 확인
+				var username = docs[0].name;
+				
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h1>로그인 성공</h1>');
+				res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
+				res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
+				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.end();
+			
+			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h1>로그인  실패</h1>');
+				res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
+				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
+		res.end();
+	}
+	
+};
+
+var adduser = function(req, res) {
+	console.log('/process/adduser 호출됨.');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    var paramName = req.body.name || req.query.name;
+	
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+    
+    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
+	if (database) {
+		addUser(database, paramId, paramPassword, paramName, function(err, addedUser) {
+            // 동일한 id로 추가하려는 경우 에러 발생 - 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 추가 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+			
+            // 결과 객체 있으면 성공 응답 전송
+			if (addedUser) {
+				console.dir(addedUser);
+ 
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가 성공</h2>');
+				res.end();
+			} else {  // 결과 객체가 없으면 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가  실패</h2>');
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+};
+
+var listuser = function(req, res) {
+	console.log('/process/listuser 호출됨.');
+
+    // 데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
+	if (database) {
+		// 1. 모든 사용자 검색
+		UserModel.findAll(function(err, results) {
+			// 에러 발생 시, 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 리스트 조회 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+			  
+			if (results) {  // 결과 객체 있으면 리스트 전송
+				console.dir(results);
+ 
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트</h2>');
+				res.write('<div><ul>');
+				
+				for (var i = 0; i < results.length; i++) {
+					var curId = results[i]._doc.id;
+					var curName = results[i]._doc.name;
+					res.write('    <li>#' + i + ' : ' + curId + ', ' + curName + '</li>');
+				}	
+			
+				res.write('</ul></div>');
+				res.end();
+			} else {  // 결과 객체가 없으면 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회  실패</h2>');
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+};
+
+module.exports.init = init;
+module.exports.login = login;
+module.exports.adduser = adduser;
+module.exports.listuser = listuser;
+
